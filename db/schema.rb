69dde4460a85bdc1f74296b6dcfaa25203f2462f
Ernestby <your.ernestby@gmail.com>
2016-10-15 09:57:16 +0600

    sign-in and sign-out

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161013084211) do
+ActiveRecord::Schema.define(version: 20161014140842) do
 
 # Could not dump table "sqlite_stat1" because of following NoMethodError
 #   undefined method `[]' for nil:NilClass
@@ -25,8 +25,10 @@ ActiveRecord::Schema.define(version: 20161013084211) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
